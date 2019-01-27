03e22db514a2e82ec3c18fb9fd77fcf571c56c93
Derrick Cheah <derrick.cheah01@gmail.com>
2019-01-27 15:59:41 -0600

    Modified service

--- YellowSnowCone/src/main/resources/YellowSnowCone/src/app/posts.service.ts
+++ YellowSnowCone/src/main/resources/YellowSnowCone/src/app/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Posts } from './posts';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class PostsService {
 
   userId: number = 1;
 
-  userPost: Posts = [];
+  userPost: Posts;
 
   constructor(
     private http: HttpClient
