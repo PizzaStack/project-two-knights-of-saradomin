a2d24bbc63bdb295b113d18cfceeb01f4b14ed07
Boyd Thurston <boydthurston7@gmail.com>
2019-01-31 14:51:31 -0600

    test

--- YellowSnowCone/src/main/resources/YellowSnowCone/src/app/user.service.ts
+++ YellowSnowCone/src/main/resources/YellowSnowCone/src/app/user.service.ts
@@ -43,6 +43,7 @@ export class UserService {
   }
 
   authenticate(user: Users): Observable<Users> {
+    alert(this._url);
     console.log('authenticating... ' + user.email + ', ' + user.password);
     return this.http.post<Users>(this._url.concat('authenticate'),
       JSON.stringify(user), httpPostOptions)
